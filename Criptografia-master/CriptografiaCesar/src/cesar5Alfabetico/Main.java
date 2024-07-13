924e26f258e98093cca420541e3ade4927625b68
JMGSJUANMA <jmgsjuanma@gmail.com>
2024-07-13 17:15:20 +0200

    tercerCommit

--- Criptografia-master/CriptografiaCesar/src/cesar5Alfabetico/Main.java
+++ Criptografia-master/CriptografiaCesar/src/cesar5Alfabetico/Main.java
@@ -4,7 +4,7 @@ public class Main {
         // Ejemplo de uso
         String mensaje = "Hola, este es un mensaje cifrado con Cesar +5!";
         String mensajeCifrado = CesarCipher5Alfab.cifrar(mensaje);
-        System.out.println("/n Comienzo del programa de cigrado: ");
+        System.out.println("Comienzo del programa de cifrado: ");
         System.out.println("Mensaje cifrado: " + mensajeCifrado);
 
         String mensajeDescifrado = CesarCipher5Alfab.descifrar(mensajeCifrado);
